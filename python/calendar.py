1324642b9b8dd46639e582f10014415086481a5f
sweetrain096 <gpfhddl09@gmail.com>
2019-01-29 17:37:53 +0900

    190129

--- python/calendar.py
+++ python/calendar.py
@@ -1,5 +1,7 @@
 import sys
 sys.stdin = open("calendar_input.txt")
+
+
 def GCD(a, b) :
     while True :
         r = a % b
@@ -14,30 +16,68 @@ for test_case in range(t):
     m, n, x, y = list(map(int, input().split()))
     gcd = GCD(n, m)
     lcm = int(n * m / gcd)
+
     # print(m, n, x, y, gcd, lcm)
-    result = -1
-    if m == x and n == y :
-        result = lcm
-    cnt = int(lcm / m)
-    # print("cnt", cnt)
-    first = x
-    if first > n:
-        first = first % n
-        if not first :
-            first = y
-    # print(first)
 
-    for month in range(cnt):
-        if first == y :
-            result = (month) * m + x
+    count = x
+    check = x
+    for cnt in range(int(lcm / m)):
+        if check == y:
             break
-        first += m
-        if first > n:
-            first = first % n
-            if not first:
-                first = n
+        count += m
+        check = (check + m) % n
+        if not check:
+            check = n
+
+    if check != y :
+        count = -1
 
 
+    print(count)
 
 
-    print(result)
+    # result = -1
+    # if m == x and n == y :
+    #     result = lcm
+    # cnt = int(lcm / m)
+    # # print("cnt", cnt)
+    # first = x
+    # if first > n:
+    #     first = first % n
+    #     if not first :
+    #         first = y
+    # # print(first)
+    #
+    # for month in range(cnt):
+    #     if first == y :
+    #         result = (month) * m + x
+    #         break
+    #     first += m
+    #     if first > n:
+    #         first = first % n
+    #         if not first:
+    #             first = n
+    #
+    #
+    #
+    #
+    # print(result)
+    #
+
+#
+# for test_case in range(int(input())):
+#
+#     M, N, x, y = map(int,input().split())
+#
+#     i=0
+#     while y != ((M * i) + x) % N:
+#         i=i+1
+#         if i>N:
+#             break
+#     Y = M * i + x
+#     if Y > M * N:
+#         print(-1)
+#     else:
+#         print(Y)
+#
+#
--- sw_expert_academy/d2/rotation_numbers.py
+++ sw_expert_academy/d2/rotation_numbers.py
@@ -10,11 +10,27 @@ for test_case in range(int(input())):
     for i in range(n):
         numbers.append(list(map(int, input().split())))
 
+    check_nums = ""
     # 90
+    for row in range(n):
+        for col in range(n):
+            check_nums += str(numbers[-(col + 1)][row])
+        rotation_90.append(check_nums)
+        check_nums = ""
 
+    # 180
+    for row in range(n):
+        check_nums += "".join(map(str, numbers[-(row+1)][::-1]))
+        rotation_180.append(check_nums)
+        check_nums = ""
 
-
-
+    # 270
+    for row in range(n):
+        for col in range(n):
+            check_nums += str(numbers[col][-(row+1)])
+        rotation_270.append(check_nums)
+        check_nums = ""
 
     print(f"#{test_case + 1}")
-    print(numbers)
+    for row in range(n):
+        print(rotation_90[row], rotation_180[row], rotation_270[row])
