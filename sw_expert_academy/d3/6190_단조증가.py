045bbf7a1603552eae3e0fd9213997170fb650bb
sweetrain096 <gpfhddl09@gmail.com>
2019-02-07 17:42:18 +0900

    190207

--- sw_expert_academy/d3/6190_단조증가.py
+++ sw_expert_academy/d3/6190_단조증가.py
@@ -4,25 +4,25 @@ sys.stdin = open("6190_input.txt")
 for tc in range(int(input())):
     n = int(input())
     numbers = list(map(int,input().split()))
-    check = 0
+    result = 0
     for number in range(n):
         if number != 0:
             mul = str(numbers[number] * numbers[number - 1])
+            if len(mul) == 1 :
+                if result < int(mul):
+                    result = int(mul)
+            else:
+                check = mul[0]
+                for n_int in mul:
+                    if int(check) <= int(n_int):
+                        check = n_int
+                    else:
+                        mul = 0
+                        break
+                if result < int(mul):
+                    result = int(mul)
 
+    if not result:
+        result = -1
 
-        # if len(numbers[number]) == 1 and number != 0 :
-        #     if mul < int(numbers[number]) * int(numbers[number - 1]):
-        #         mul = int(numbers[number]) * int(numbers[number - 1])
-        # else:
-        #     check = numbers[number][0]
-        #     for n_int in numbers[number]:
-        #         if check > n_int:
-        #             numbers[number] = 0
-        #     if mul < int(numbers[number]) * int(numbers[number - 1]):
-        #         mul = int(numbers[number]) * int(numbers[number - 1])
-        #
-        # if mul == 0:
-        #     mul = -1
-
-
-    print(f"#{tc + 1} {numbers} {mul}")
+    print(f"#{tc + 1} {numbers} {result}")
