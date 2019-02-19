61d1842de8b31eab1acd2a0ca05af57bf9e1e364
sweetrain096 <gpfhddl09@gmail.com>
2019-02-19 09:56:36 +0900

    190219|d3|메모리복구

--- sw_expert_academy/d3/1289_메모리복구.py
+++ sw_expert_academy/d3/1289_메모리복구.py
@@ -2,13 +2,21 @@ import sys
 sys.stdin = open("1289_input.txt")
 
 for tc in range(int(input())):
-    strings = input()
+    strings = list(input())
+    result = 0
     for char in range(len(strings)):
         if 1 + char != len(strings):
-            if strings[-(1 + char)] != strings[-(2 + char)] and strings[-(2 + char)]=='1':
-                print(strings[-(2 + char)], len(strings[-(1 + char):]), strings[-(2 + char)] * len(strings[-(1 + char):]))
-                strings[-(1 + char):] = strings[-(2 + char)] * len(strings[-(1 + char):])
-
-
-        print(strings)
-    print(f"#{tc + 1}")
+            if strings[-(1 + char)] != strings[-(2 + char)]:
+                if strings[-(2 + char)] == '1':
+                    # print(strings[-(2 + char)], len(strings[-(1 + char):]), strings[-(2 + char)] * len(strings[-(1 + char):]))
+                    strings[-(1 + char):] = strings[-(2 + char)] * len(strings[-(1 + char):])
+                    result += 1
+                else:
+                    # print(strings[-(2 + char)], len(strings[-(1 + char):]), strings[-(2 + char)] * len(strings[-(1 + char):]))
+                    strings[-(1 + char):] = strings[-(2 + char)] * len(strings[-(1 + char):])
+                    result += 1
+        if strings[-(1 + char)] == '1' and (1 + char) == len(strings):
+            strings[:] = '0' * (1 + char)
+            result += 1
+        # print(strings)
+    print(f"#{tc + 1} {result}")
