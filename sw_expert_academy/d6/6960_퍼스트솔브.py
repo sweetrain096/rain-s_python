06b8fd1786eefe80628c8c73a28ade9c30268ca0
sweetrain096 <gpfhddl09@gmail.com>
2019-02-19 17:49:38 +0900

    190219|d6|퍼스트솔브

---
+++ sw_expert_academy/d6/6960_퍼스트솔브.py
@@ -0,0 +1,9 @@
+import sys
+sys.stdin = open("6960_input.txt")
+
+for tc in range(int(input())):
+    n = int(input())
+    for test_case in range(n):
+        s, f = list[map(int, input().split())]
+
+    print(f"#{tc + 1}")
