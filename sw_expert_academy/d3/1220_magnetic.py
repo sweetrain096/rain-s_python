953fe446f6a6d7943496e37bc2ddaf19e6230e60
sweetrain096 <gpfhddl09@gmail.com>
2019-02-20 16:54:55 +0900

    190220|d3|magnetic

--- sw_expert_academy/d3/1220_magnetic.py
+++ sw_expert_academy/d3/1220_magnetic.py
@@ -2,18 +2,21 @@ import sys
 sys.stdin = open("1220_input.txt")
 for tc in range(10):
     n = input()
-    base = []
-    for row in range(100):
-        base.append(list(map(int, input().split())))
+    table = []
+    for input_table in range(100):
+        table.append(list(map(int, input().split())))
     result = 0
     for col in range(100):
-        plus_0, plus_1, plus_2 = 0, 0, 0
+        last = -1
+        now = -1
         for row in range(100):
-            now = base[row][col]
-            if now == 1:
-                plus_1 += 1
-            elif now == 2:
-                plus_2 += 1
-            last = base[row][col]
+            if table[row][col]:
+                now = table[row][col]
+            if last == 1 and now == 2:
+                result += 1
 
-    print(f"#{tc + 1} ")
+
+            last = now
+
+
+    print(f"#{tc + 1} {result}")
