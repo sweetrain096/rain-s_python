c556dd3e78a8da5625a2d1fbc12d96ebc52444c6
sweetrain096 <gpfhddl09@gmail.com>
2019-02-20 09:15:09 +0900

    190220|workshop 작업순서

--- algorism/4week/workshop_2.py
+++ algorism/4week/workshop_2.py
@@ -2,75 +2,24 @@ import sys
 sys.stdin = open("workshop2_input.txt")
 
 
-for tc in range(2):
-    v, e = map(int, input().split())
-    tmp = list(map(int, input().split()))
-    graph = [[0 for i in range(v + 1)] for j in range(v + 1)]
-    visited = [0 for i in range(v + 1)]
-    degree = [0 for i in range(v + 1)]
+for cn in range(10):
+    V, E = map(int, input().split()) # V는 정점의 갯수, E는 노드의 갯수
+    n = list(map(int, input().split()))
+    G = [[0 for _ in range(V)] for _ in range(V)]
+    for i in range(E):
+        G[n[2*i +1] - 1][n[2*i] - 1] = 1
+        line = []
+    while(1):
+        for i in range(1,V+1):
+            if sum(G[i-1]) == 0 and str(i) not in line:
+                line.append(str(i))
+                for j in range(1,V+1):
+                    G[j-1][i-1] = 0
+        if len(line) == V:
+            break
+    result = ' '.join(line)
+    print(f"#{cn+1} {result}")
 
-
-    for cnt in range(e):
-        row, col = tmp[cnt * 2 : (cnt + 1) * 2]
-        # print(row, col)
-        graph[row][col] = 1
-    # print(tmp)
-
-
-    stack = []
-    result = []
-
-    for row in range(1, v + 1):
-        for col in range(1, v + 1):
-            if graph[col][row]:
-                degree[row] += 1
-    for check in range(1, v + 1):
-        if not degree[check]:
-            stack.append(check)
-    # print(degree)
-    # print(stack)
-
-
-    while stack:
-        print("stack", stack)
-        print("result", result)
-        print("visited", visited)
-        print("degree", degree)
-        node = stack.pop()
-        if not visited[node]:
-
-            for col in range(1, v + 1):
-                if not degree[col]:
-                    result.append(col)
-                    degree[col] -= 1
-                if graph[node][col]:
-
-                    stack.append(col)
-                    degree[col] -= 1
-            check = []
-            for row in range(1, v + 1):
-                if graph[row][node]:
-                    check.append(row)
-            #
-            # # print("node", node, check)
-            # if not check:
-            #     result.append(node)
-            #     visited[node] = 1
-            # else:
-            #     cnt = 0
-            #     for check_one in check:
-            #         if visited[check_one]:
-            #             cnt += 1
-            #
-            #     if cnt == len(check):
-            #         result.append(node)
-            #         visited[node] = 1
-
-
-
-    # for graph1 in graph:
-    #     print(graph1)
-    print(f"#{tc + 1} {' '.join(map(str, result))}")
 
 
 
@@ -134,3 +83,7 @@ for tc in range(2):
 #     # for graph1 in graph:
 #     #     print(graph1)
 #     print(f"#{tc + 1} {' '.join(map(str, result))}")
+
+
+
+
