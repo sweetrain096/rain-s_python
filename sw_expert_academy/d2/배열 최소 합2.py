99c075d4465435b340d87158aacf7d0ad7f3b15b
sweetrain096 <gpfhddl09@gmail.com>
2019-02-22 17:47:48 +0900

    190222|배열 최소 합

--- sw_expert_academy/d2/배열 최소 합2.py
+++ sw_expert_academy/d2/배열 최소 합2.py
@@ -1,24 +1,30 @@
 import sys
 sys.stdin = open("4881_input.txt")
 
-MAXCANDIDATES = 100
-NMAX = 100
-
 def process_solution(a, k, sum_data):
-    if sum_data != 10:
+    global result
+    if sum_data < result:
+        result = sum_data
+    else:
         return
     for i in range(1, k + 1):
         if a[i]:
-            print(table[i], end=" ")
+            print(data[i][a[i]], end=" ")
     print()
 
 def make_candidates(a, k, input, c):
-    c[0] = True
-    c[1] = False
-    return 2
+    c[0] = 1
+    c[1] = 2
+    c[2] = 3
+    return 3
 
 def backtrack(a, k, input, sum_data):
-    if sum_data > 10:
+    print(a)
+    global result
+    print('sum', sum_data, a)
+    visited = []
+
+    if sum_data > result:
         return
     global MAXCANDIDATES
     c = [0] * MAXCANDIDATES
@@ -32,20 +38,31 @@ def backtrack(a, k, input, sum_data):
             a[k] = c[i]
             # 가지치기. a[k]가 1일 때 data[k]의 값을 확인하여 더한다.
             if a[k]:
-                backtrack(a, k, input, sum_data + table[k])
+                visited.append(a[k])
+                backtrack(a, k, input, sum_data + data[k][a[k]])
+
             else:
                 backtrack(a, k, input, sum_data)
 
 
 
+# 임의의 값(공간)을 미리 확보
+
+
+MAXCANDIDATES = 100
+NMAX = 100
+
+
+
 for tc in range(int(input())):
     n = int(input())
-    table = [0] * n
-    for row in range(n):
-        table[row] = [0] + list(map(int, input().split()))
-    table.insert(0, [0] * (n + 1))
+    data = [0] * n
 
-    for row in table:
+    for row in range(n):
+        data[row] = [0] + list(map(int, input().split()))
+    data.insert(0, [0] * (n + 1))
+
+    for row in data:
         print(row)
 
 
