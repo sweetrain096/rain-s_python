8dea9654bb2fb81d3a304f110cd862567e79432e
sweetrina096 <gpfhddl09@gmail.com>
2019-02-23 13:24:51 +0900

    1190223|배열최소합

--- sw_expert_academy/d2/배열 최소 합2.py
+++ sw_expert_academy/d2/배열 최소 합2.py
@@ -3,25 +3,46 @@ sys.stdin = open("4881_input.txt")
 
 def process_solution(a, k, sum_data):
     global result
+    check_list = [0] * (k + 1)
+    for i in range(1, k + 1):
+        if a[i]:
+            check_list[a[i]] += 1
+            if check_list[a[i]] >= 2:
+                return
+    # print(check_list)
+
     if sum_data < result:
         result = sum_data
     else:
         return
-    for i in range(1, k + 1):
-        if a[i]:
-            print(data[i][a[i]], end=" ")
-    print()
+
+    # for i in range(1, k + 1):
+    #     if a[i]:
+    #         print(data[i][a[i]], end=" ")
+    # print()
 
 def make_candidates(a, k, input, c):
-    c[0] = 1
-    c[1] = 2
-    c[2] = 3
-    return 3
+    global n
+    for i in range(n):
+        c[i] = i + 1
+
+    checklist = [0] * (n + 1)
+    for i in range(1, n + 1):
+        checklist[a[i]] += 1
+
+
+    for i in range(1, n + 1):
+        if checklist[i] > 2:
+            c[i - 1] = 0
+
+
+
+
+    return n
 
 def backtrack(a, k, input, sum_data):
-    print(a)
     global result
-    print('sum', sum_data, a)
+    # print('sum', sum_data, a)
     visited = []
 
     if sum_data > result:
@@ -39,10 +60,11 @@ def backtrack(a, k, input, sum_data):
             # 가지치기. a[k]가 1일 때 data[k]의 값을 확인하여 더한다.
             if a[k]:
                 visited.append(a[k])
-                backtrack(a, k, input, sum_data + data[k][a[k]])
+                backtrack(a, k, input, sum_data + data[k-1][a[k]-1])
 
             else:
-                backtrack(a, k, input, sum_data)
+                # backtrack(a, k, input, sum_data)
+                return
 
 
 
@@ -56,14 +78,16 @@ NMAX = 100
 
 for tc in range(int(input())):
     n = int(input())
-    data = [0] * n
+    data = []
 
     for row in range(n):
-        data[row] = [0] + list(map(int, input().split()))
-    data.insert(0, [0] * (n + 1))
+        data.append(list(map(int, input().split())))
+    # for row in range(n):
+    #     data[row] = [0] + list(map(int, input().split()))
+    # data.insert(0, [0] * (n + 1))
 
-    for row in data:
-        print(row)
+    # for row in data:
+    #     print(row)
 
 
     a = [0 for _ in range(n + 1)]
