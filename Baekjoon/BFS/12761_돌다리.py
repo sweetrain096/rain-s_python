60985c9bb613a3af5c87d095c93b851b213723f3
sweetrain096 <gpfhddl09@gmail.com>
2019-03-05 17:51:24 +0900

    190305|BFS

--- Baekjoon/BFS/12761_돌다리.py
+++ Baekjoon/BFS/12761_돌다리.py
@@ -1,19 +1,55 @@
 import sys
-sys.stdin = open("2761_input.txt")
+sys.stdin = open("12761_input.txt")
+
+def check(number):
+    if number >= 0 and number <= 100000:
+        if not visited[number]:
+            return True
+    return False
+
 
 def bfs(a, b, n, m):
-    Q = []
-    if n - 1 >= 0:
-        Q.append(n - 1)
-    if n + 1 <= 100000:
-        Q.append(n + 1)
-    if n * a <= 100000:
-        Q.append(n + a)
-        Q.append(n * a)
-    if n * b <= 100000:
-        Q.append(n + b)
-        Q.append(n * b)
+    Q = [n]
+    visited[n] = 1
+    check_cnt = Q[-1]
+    cnt = 0
+    order = ["-1", "+1"]
+    while cnt <= 1:
+        t = Q.pop(0)
+        if t == 
+        for i in order:
+            if check(t + int(i)):
+                Q.append(t + int(i))
+                visited[t + int(i)] = 1
+        if check(t + a):
+            Q.append(t + a)
+            visited[t + a] = 1
+        if check(t - a):
+            Q.append(t - a)
+            visited[t - a] = 1
+        if check(t * a):
+            Q.append(t * a)
+            visited[t * a] = 1
+        if check(t + b):
+            Q.append(t + b)
+            visited[t + b] = 1
+        if check(t - b):
+            Q.append(t - b)
+            visited[t - b] = 1
+        if check(t * b):
+            Q.append(t * b)
+            visited[t * b] = 1
+
+        if check_cnt == t:
+            cnt += 1
+            print(check_cnt, Q[-1])
+            check_cnt = Q[-1]
+
+        print(Q)
+        print(cnt)
+
 
 
 a, b, n, m = map(int, input().split())
-visited = []
+visited = [0 for _ in range(100001)]
+bfs(a, b, n, m)
---
+++ Baekjoon/BFS/test.py
@@ -0,0 +1,7 @@
+
+print("1 + 2")
+a = 3
+b = 4
+# print("{}%d".format(5+int("a")))
+
+print("%d " %(-a+b))
