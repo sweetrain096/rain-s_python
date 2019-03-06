14f779e92f34a6541c9803c4bf14930a5431624e
sweetrina096 <gpfhddl09@gmail.com>
2019-03-06 22:46:37 +0900

    190306|BFS|단지번호붙이기

--- Baekjoon/BFS/2667_단지번호붙이기.py
+++ Baekjoon/BFS/2667_단지번호붙이기.py
@@ -11,12 +11,13 @@ for i in range(n):
     data.append(list(map(int, input().strip())))
 
 Q = []
-total = 0
 result = []
 for row in range(n):
     for col in range(n):
         if data[row][col]:
             Q.append([row, col])
+            data[row][col] = 0
+            total = 1
             while Q:
                 r, c = Q.pop(0)
                 if not iswall(r + 1, c) and data[r + 1][c]:
@@ -36,7 +37,7 @@ for row in range(n):
                     data[r][c - 1] = 0
                     total += 1
             result.append(total)
-            total = 0
+
 result.sort()
 print(len(result))
 for i in result:
