70b64a081582fad5f365c640623378f3a78459b8
sweetrain096 <gpfhddl09@gmail.com>
2019-03-12 17:56:19 +0900

    190312|N-Queen

--- Baekjoon/backtracking/9663_N-Queen.py
+++ Baekjoon/backtracking/9663_N-Queen.py
@@ -5,46 +5,132 @@ def iswall(r, c):
     if r < 0 or r >= n or c < 0 or c >= n:
         return True
 
-def is_not_safe(now_r, now_c):
+def make_wall(now_r, now_c, board):
+    # print(now_r, now_c)
     for pr, pc in deg:
+        row, col = now_r, now_c
         while True:
-            now_r += pr
-            now_c += pc
-            if not iswall(now_r, now_c) and board[now_r][now_c]:
-                return False
-            if iswall(now_r, now_c):
+            row += pr
+            col += pc
+            # print(row, col)
+            if not iswall(row, col) and board[row][col]:
+                board[row][col] = 0
+            if iswall(row, col):
                 break
+    return True
 
-def dfs(r, c, cnt):
+def dfs(r, c, board):
     global result
-    if not board[r][c]:
-        board[r][c] = 1
-        cnt += 1
-    if cnt == n:
-        result += 1
-        print(result)
-        return
+    if board[r][c]:
+        visited[r] = 1
+        make_wall(r, c, board)
 
-    for row in range(r + 1, n):
-        for col in range(c, n):
+    for i in board:
+        print(i)
+    print()
 
-            for i in board:
-                print(i)
-            print()
-            if not is_not_safe(row, col):
-                dfs(row, col, cnt)
+    # if sum(visited) == n:
+    #     result += 1
+    #     # for i in board:
+    #     #     print(i)
+    #     # print()
+    #     # print("result", result)
+    #     # return
+    #
+    # for row in range(r + 1, n):
+    #     for col in range(n):
+    #         if row and not visited[row - 1]:
+    #             break
+    #
+    #         if is_safe(row, col, board):
+    #             dfs(row, col, board)
+    #
+    #         for i in board:
+    #             print(i)
+    #         print()
+    #
+    # # print('last', r, c)
     # board[r][c] = 0
+    # visited[r] = 0
 
 
 
-
-
-
+# deg = [[0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1]]
 deg = [[0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1]]
 n = int(input())
-board = [[0 for _ in range(n)] for _ in range(n)]
+board = [[1 for _ in range(n)] for _ in range(n)]
 result = 0
+visited = [0] * n
 
-dfs(0, 0, 0)
+for i in range(n):
+    for j in board:
+        print(j)
+    print()
+    dfs(0, i, board)
+
 
 print(result)
+
+
+
+
+
+# def iswall(r, c):
+#     if r < 0 or r >= n or c < 0 or c >= n:
+#         return True
+#
+# def is_safe(now_r, now_c):
+#     # print(now_r, now_c)
+#     for pr, pc in deg:
+#         row, col = now_r, now_c
+#         while True:
+#             row += pr
+#             col += pc
+#             # print(row, col)
+#             if not iswall(row, col) and board[row][col]:
+#                 return False
+#             if iswall(row, col):
+#                 break
+#     return True
+#
+# def dfs(r, c):
+#     global result
+#     if not board[r][c]:
+#         board[r][c] = 1
+#         visited[r] = 1
+#     if sum(visited) == n:
+#         result += 1
+#         # for i in board:
+#         #     print(i)
+#         # print()
+#         # print("result", result)
+#         # return
+#
+#     for row in range(r + 1, n):
+#         for col in range(n):
+#             if row and not visited[row - 1]:
+#                 break
+#
+#             if is_safe(row, col):
+#                 dfs(row, col)
+#
+#             for i in board:
+#                 print(i)
+#             print()
+#
+#     # print('last', r, c)
+#     board[r][c] = 0
+#     visited[r] = 0
+#
+#
+#
+# deg = [[0, -1], [-1, -1], [-1, 0], [-1, 1], [0, 1]]
+# n = int(input())
+# board = [[0 for _ in range(n)] for _ in range(n)]
+# result = 0
+# visited = [0] * n
+#
+# for i in range(n):
+#     dfs(0, i)
+#
+# print(result)
