9c19bf47981519609eb295ea533c5719367b5522
sweetrina096 <gpfhddl09@gmail.com>
2019-03-20 23:31:02 +0900

    190320|암호코드스캔

--- algorism/9week/암호코드스캔.py
+++ algorism/9week/암호코드스캔.py
@@ -1,6 +1,35 @@
 import sys
 sys.stdin = open("workshop2_input.txt")
 
+def find_mul(j):
+    global mul
+
+    for check_cnt in range(2, 9):
+        check_mul = []
+        now = j
+        cnt_mul = 0
+        flag = False
+        for i in range(7):
+            for plus in range(1, check_cnt):
+                if data[-now] != data[-(now + plus)]:
+                    flag = True
+                    break
+            check_mul.append(data[-now])
+            now += check_cnt
+            cnt_mul += 1
+
+            if flag:
+                break
+        if cnt_mul == 7:
+            mul = check_cnt
+
+    if not mul:
+        mul = 1
+
+
+
+
+
 asc = [[0,0,0,0],
         [0,0,0,1],
         [0,0,1,0],
@@ -28,25 +57,19 @@ patterns = [[0, 0, 0, 1, 1, 0, 1],
             [0, 1, 1, 1, 0, 1, 1],
             [0, 1, 1, 0, 1, 1, 1],
             [0, 0, 0, 1, 0, 1, 1]]
-# check_mul = [[0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1],
-#              [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1],
-#              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1],
-#              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
-#              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1],
-#              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1],
-#              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1]
-#              ]
 
 for tc in range(int(input())):
     r, c = map(int, input().split())
     result = []
-    before = []
+    before = ""
     for i in range(r):
-        input_data = list(input())
+        input_data = input()
         if before != input_data:
             before = input_data
-            mul = 0
+            while_cnt = len([x for x in input_data.split('000') if len(x) > 4])
+            input_data = list(input_data)
             data = []
+            # print('while_cnt', while_cnt)
             for i in input_data:
                 if ord(i) >= 65:
                     data += asc[ord(i) - 55]
@@ -55,67 +78,94 @@ for tc in range(int(input())):
 
             if 1 in data:
                 total = len(data)
-                print(data)
-                cnt = 0
+                # print(data)
                 j = 1
-                while True:
-                    if j >= total:
-                        break
-                    # if cnt and not cnt % 7:
 
-                    if data[-j]:
-                        # for now_pattern in range(10):
-                        #     if patterns[now_pattern] == data[-(j + 7 - 1) : -(j - 1)]:
-                        #         result.append(now_pattern)
-                        #         cnt += 1
-                        #         j += 7
-                        #         mul = 1
-                        #         break
-                        if not mul:
-                            now = j
-                            cnt_mul = 0
-                            for check_cnt in range(2, 9):
-                                check_mul = []
-                                for plus in range(1, check_cnt):
-                                    if data[-now] != data[-(now + plus)]:
-                                        break
-                                    now += check_cnt
-                                    cnt_mul += 1
-                                    check_mul.append(data[-now])
-                                if cnt_mul == 7:
-                                    mul = check_cnt
-                                    check_mul = check_mul[::-1]
-                                    break
-                            if not mul:
-                                mul = 1
-                        print(check_mul)
-                        if mul:
+                for while_cnt_check in range(while_cnt):
+                    cnt = 0
+                    mul = 0
+                    tmp = []
+
+                    while True:
+                        if j >= total:
+                            break
+                        if cnt == 8:
+                            break
+                        # if cnt and not cnt % 7:
+                        if data[-j]:
+                            check_mul = find_mul(j)
+                            # print(mul)
+
                             for now_pattern in range(10):
-                                if patterns[now_pattern] == check_mul[-(j + 7 - 1): -(j - 1)]:
-                                    result.append(now_pattern)
+                                flag = False
+                                for plus in range(7):
+                                    if patterns[now_pattern][-(plus + 1)] != data[-(j + plus * mul)]:
+                                        flag = True
+                                        break
+                                if not flag:
+                                    # print(patterns[now_pattern])
+                                    # result.append(now_pattern)
+                                    tmp.append(now_pattern)
+                                    j += 7 * mul
                                     cnt += 1
-                                    j += 7
-                                    mul = 1
                                     break
+                        else:
+                            j += 1
+
+                    tmp = tmp[::-1]
+                    if tmp not in result and tmp:
+                        result.append(tmp)
 
 
-                                # check_mul[-(j + (check_cnt) - 1) : -(j - 1)]
 
 
-                    else:
-                        j += 1
+    # result = result[::-1]
+
+    # print(len(result), result)
+    plus_even, plus_odd, check = 0, 0, 0
+    final = 0
+
+    for i in range(len(result)):
+        for j in range(8):
+            if not j % 2:
+                plus_even += result[i][j]
+            else:
+                plus_odd += result[i][j]
+
+        check = plus_even * 3 + plus_odd
+        # print(check)
+        if not check % 10:
+            final += sum(result[i])
+        check, plus_even, plus_odd = 0, 0, 0
+
+    print("#{} {}".format(tc + 1, final))
+    # if not check % 10:
+    #     print("#{} {}".format(tc + 1, sum(result)))
+    # else:
+    #     print("#{} {}".format(tc + 1, 0))
+
+# for now_pattern in range(10):
+#     if patterns[now_pattern] == data[-(j + 7 - 1) : -(j - 1)]:
+#         result.append(now_pattern)
+#         cnt += 1
+#         j += 7
+#         mul = 1
+#         break
 
 
-    result = result[::-1]
-    plus_even, plus_odd = 0, 0
-    for i in range(8):
-        if not i % 2:
-            plus_even += result[i]
-        else:
-            plus_odd += result[i]
-
-    check = plus_even * 3 + plus_odd
-    if not check % 10:
-        print("#{} {}".format(tc + 1, sum(result)))
-    else:
-        print("#{} {}".format(tc + 1, 0))
+# now = j
+# cnt_mul = 0
+# for check_cnt in range(2, 9):
+#     check_mul = []
+#     for plus in range(1, check_cnt):
+#         if data[-now] != data[-(now + plus)]:
+#             break
+#         now += check_cnt
+#         cnt_mul += 1
+#         check_mul.append(data[-now])
+#     if cnt_mul == 7:
+#         mul = check_cnt
+#         check_mul = check_mul[::-1]
+#         break
+# if not mul:
+#     mul = 1
