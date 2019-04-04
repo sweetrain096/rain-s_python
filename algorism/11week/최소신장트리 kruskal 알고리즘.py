4ce0d700509de4f551edd4a29c5b2f52cc7627f1
sweetrain096 <gpfhddl09@gmail.com>
2019-04-04 15:36:23 +0900

    190404|알고리즘

--- algorism/11week/최소신장트리 kruskal 알고리즘.py
+++ algorism/11week/최소신장트리 kruskal 알고리즘.py
@@ -1,23 +1,37 @@
+import sys
+sys.stdin = open("최소신장트리_input.txt")
 
-def mst_kruskal(G, w):
-    A = []
-    for vertex in G[0]:
-        Make_Set(v)
+def find_set(x):
+    if x == p[x] : return x
+    else : return find_set(p[x])
 
-    # 정렬
+def make_mst():
+    cnt = 0             # 간선 개수
+    result = 0          # 가중치의 합
 
-    #가중치 낮은 간선 선택
+    i = 0                           # 간선 관리
+    while cnt < node_total:         # 트리에서 간선 개수는 노드 - 1
+        p1 = find_set(graph[i][0])  # 가중치가 가장 작은 노드(순차적으로 내려간다.)의
+        p2 = find_set(graph[i][1])  # 두 노드의 대표 원소를 p1, p2에서 찾는다.
+        if p1 != p2:
+            result += graph[i][2]
+            cnt += 1
+            p[p2] = p1              # 대표 원소를 하나로 합친다.
+        i += 1                      # 다음 간선으로 이동
+    return result
 
 
-G = [[5, 3, 18],
-     [1, 2, 21],
-     [2, 6, 25],
-     [0, 2, 31],
-     [0, 1, 32],
-     [3, 4, 34],
-     [5, 4, 40],
-     [2, 4, 46],
-     [0, 6, 51],
-     [4, 6, 51],
-     [0, 5, 60]]
+for tc in range(int(input())):
+    node_total, edge_total = map(int, input().split())
+
+    graph = [list(map(int, input().split())) for i in range(edge_total)]
+    graph.sort(key=lambda x : x[2])         # 가중치 기준 오름차순 정렬
+    p = list(range(node_total + 1))         # make set. 대표 원소 초기화
+
+    print(p)
+    for i in graph:
+        print(i)
+
+
+    print("#{} {}".format(tc + 1, make_mst()))
 
--- algorism/11week/최소이동거리 djikstra.py
+++ algorism/11week/최소이동거리 djikstra.py
@@ -1,6 +1,27 @@
 import sys
 sys.stdin = open("최소이동거리_input.txt")
 
+def djikstra(start_node):
+    key[start_node] = 0
+    result = 0
+
+    for i in range(end_node + 1):
+        min_weight = 9999999
+        for v in range(end_node + 1):
+            if not visited[v] and min_weight > (key[v]):
+                min_weight = key[v]
+                start_node = v
+        visited[start_node] = 1
+
+        for v in range(end_node + 1):
+            if graph[start_node][v] and not visited[v] and graph[start_node][v] + key[start_node] < key[v]:
+                key[v] = graph[start_node][v] + key[start_node]
+                parent[v] = start_node
+
+
+    return key[end_node]
+
+
 for tc in range(int(input())):
     end_node, edge_total = map(int, input().split())
     graph = [[0 for _ in range(end_node + 1)] for _ in range(end_node + 1)]
@@ -9,4 +30,11 @@ for tc in range(int(input())):
         s, e, w = map(int, input().split())
         graph[s][e] = w
 
+    key = [9999999] * (end_node + 1)
+    parent = list(range(end_node + 1))
+    visited = [0] * (end_node + 1)
 
+    # for i in graph:
+    #     print(i)
+
+    print("#{} {}".format(tc + 1, djikstra(0)))
