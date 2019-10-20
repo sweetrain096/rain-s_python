8c73582af7be5ec858626622234d03e16a5e6c98
sweetrina096 <gpfhddl09@gmail.com>
2019-10-20 15:00:25 +0900

    BFS|안전영역|BJ

--- Baekjoon/BFS/2468_안전영역.py
+++ Baekjoon/BFS/2468_안전영역.py
@@ -1,7 +1,6 @@
 import sys
 sys.stdin = open("2468_input.txt")
 
-import copy
 
 
 deg = [(-1, 0), (1, 0), (0, -1), (0, 1)]
@@ -11,34 +10,27 @@ def is_wall(r, c):
         return True
     return False
 
-def bfs(rain, Map):
-    cnt = -1
+def bfs(rain):
+    visited = [[0 for _ in range(n)] for _ in range(n)]
+    cnt = 0
     for r in range(n):
         for c in range(n):
-            if Map[r][c] > rain:
-                print(r, c)
+            if Map[r][c] > rain and not visited[r][c]:
+                cnt += 1
                 Q.append([r, c])
+                visited[r][c] = 1
                 while Q:
                     tr, tc = Q.pop(0)
-                    Map[tr][tc] = cnt
                     for dr, dc in deg:
                         nr = tr + dr
                         nc = tc + dc
-                        if not is_wall(nr, nc) and not Map[nr][nc]:
+                        if not is_wall(nr, nc) and not visited[nr][nc] and Map[nr][nc] > rain:
                             Q.append([nr, nc])
-                cnt -= 1
-    return -cnt - 1
+                            visited[nr][nc] = 1
 
-# def rainning(rain, Map):
-#     for r in range(n):
-#         for c in range(n):
-#             if Map[r][c] <= rain:
-#                 Map[r][c] = -1
-#             else:
-#                 Map[r][c] = 0
-#
-#     a = bfs(Map)
-#     return a
+    return cnt
+
+
 
 n = int(input())
 Map = [list(map(int, input().split())) for _ in range(n)]
@@ -47,9 +39,7 @@ for line in Map:
     final = max(final, max(line))
 safe = 1
 
-for rain in range(2, 101):
-    if rain > final:
-        break
-    safe = max(safe, bfs(rain, copy.deepcopy(Map)))
+for rain in range(1, final+1):
+    safe = max(safe, bfs(rain))
 
 print(safe)
