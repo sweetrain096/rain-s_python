64d4fc9ab6da0317fac311ef928e968722a5f5d1
sweetrina096 <gpfhddl09@gmail.com>
2019-12-20 20:53:30 +0900

    simulation

--- sw_expert_academy/d3/4789_연극.py
+++ sw_expert_academy/d3/4789_연극.py
@@ -3,3 +3,13 @@ sys.stdin = open("4789_input.txt")
 
 for tc in range(int(input())):
     test = input()
+    result = 0
+    sum_people=0
+    for i in range(len(test)):
+        if int(test[i]) and sum_people < i:
+            result += i - sum_people
+            sum_people += i - sum_people
+
+        sum_people += int(test[i])
+
+    print(f"#{tc + 1} {result}")
