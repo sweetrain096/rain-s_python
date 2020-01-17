a80eebc4e68e41b13d38b4953214a53f030ccadf
sweetrina096 <gpfhddl09@gmail.com>
2020-01-17 14:41:36 +0900

    mutable immutable

---
+++ posts/mutable_immutable.py
@@ -0,0 +1,43 @@
+# num = 1
+# print(num)
+#
+# num = 2
+# print(num)
+
+# string = "abc"
+# print(string)
+# string = "hi"
+# print(string)
+
+# string = "abc"
+# print(string)
+# string[-1] = "z"
+# print(string)
+
+# tuple_test = (1, 2, 3)
+# print(tuple_test)
+# tuple_test[0] = 10
+# print(tuple_test)
+
+# arr = [1, 2, 3]
+# print(arr)
+# arr[0] = 10
+# print(arr)
+
+# dictionary = dict()
+# dictionary["q"] = (1, 2, 3)
+# print(dictionary)
+# dictionary[1] = "1입니다"
+# print(dictionary)
+# dictionary[[1, 2, 3]] = 1
+# print(dictionary)
+
+arr = [1, 2, 3]
+arr[0] = 100
+print(arr)
+
+string = "abc"
+string.replace("a", "z")
+print(string)
+string = string.replace("a", "z")
+print(string)
