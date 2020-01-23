16533d0e84526d5808974209a7c9f5d47ded1a04
sweetrina096 <gpfhddl09@gmail.com>
2020-01-23 20:48:00 +0900

    단순구현

--- sw_expert_academy/d4/6959_덧셈게임.py
+++ sw_expert_academy/d4/6959_덧셈게임.py
@@ -2,10 +2,26 @@ import sys
 sys.stdin = open("6959_input.txt")
 
 for tc in range(int(input())):
-    testcase = list(input())
+    start = 0
+    testcase = list(map(int, list(input())))
+    # print(testcase)
     while len(testcase) > 1:
-        for i in range(1, len(testcase)):
-            if not int(testcase[i - 1]) + int(testcase[i]):
+        temp = 20
+        idx = 0
+        for i in range(len(testcase) - 1):
+            if temp > testcase[i] + testcase[i + 1]:
+                temp = testcase[i] + testcase[i + 1]
+                idx = i
+        plus = testcase[idx] + testcase[idx + 1]
+        testcase.pop(idx)
+        testcase.pop(idx)
+        if plus >= 10:
+            testcase.insert(idx, 1)
+            testcase.insert(idx + 1, plus % 10)
+        else:
+            testcase.insert(idx, plus)
+        # print(testcase, start)
+        start = (start + 1) % 2
 
 
-    print(f"#{tc + 1} {testcase}")
+    print(f"#{tc + 1} {'A' if start else 'B'}")
