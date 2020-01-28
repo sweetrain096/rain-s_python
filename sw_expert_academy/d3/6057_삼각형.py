afb2882fe188ed14e7c28b4b79dbc280a7904ee1
sweetrina096 <gpfhddl09@gmail.com>
2020-01-28 09:54:35 +0900

    삼각형

--- sw_expert_academy/d3/6057_삼각형.py
+++ sw_expert_academy/d3/6057_삼각형.py
@@ -1,35 +1,32 @@
 import sys
 sys.stdin = open("6057_input.txt")
 
+
+def make_triangle(start, mid):
+    global cnt
+    for i in range(mid + 1, n + 1):
+        if graph[mid][i]:
+            if graph[i][start]:
+                end = i
+                able = tuple(sorted([start, mid, end]))
+                if not make.get(able):
+                    make[able] = 1
+                    cnt += 1
+
+
 for tc in range(int(input())):
     n, m = map(int, input().split())
-    graph = [[] for _ in range(n + 1)]
+    graph = [[0 for _ in range(n + 1)] for _ in range(n + 1)]
+    make = dict()
+    cnt = 0
     for i in range(m):
         x, y = map(int, input().split())
-        graph[x].append(y)
-        graph[y].append(x)
-    print(graph)
+        graph[x][y] = 1
+        graph[y][x] = 1
 
-    idx = 1
-    for i in range(1, n + 1):
-        if graph[i]:
-            idx = i
-            break
-
-    while True:
-        v = idx
-        flag = True
-        for i in range(3):
-            print("v", v)
-            Next = graph[v].pop()
-            print(Next, graph)
-            graph[Next].remove(v)
-            print(graph)
-            if not graph[Next]:
-                flag = False
-                break
-
-            v = graph[Next].pop()
-            print(Next, v)
-
-        break
+    for r in range(1, n + 1):
+        for c in range(r + 1, n + 1):
+            if graph[r][c]:
+                make_triangle(r, c)
+            # print(r, c, cnt)
+    print(f"#{tc + 1} {cnt}")
