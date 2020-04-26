b76c26150a0e8798c7e21174fffbd95ea1ee8d3e
sweetrina096 <gpfhddl09@gmail.com>
2020-04-27 00:01:00 +0900

    백트레킹
    if문으로 result 처리하니 시간초과

--- Baekjoon/DFS/1987_알파벳.py
+++ Baekjoon/DFS/1987_알파벳.py
@@ -1,22 +1,26 @@
 import sys
 sys.stdin = open("1987_input.txt")
 
-def iswall(row, col):
+deg = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+def is_wall(row, col):
     if row < 0 or row >= r or col < 0 or col >= c:
         return True
-def dfs(node, visited):
-    global cnt, result
+def dfs(node, visited, cnt):
+    global result
     row, col = node[0], node[1]
     alpha = ord(board[row][col]) - 65
+
     visited[alpha] = 1
     cnt += 1
     result = max(cnt, result)
 
-    for plus_r, plus_c in deg:
-        if not iswall(row + plus_r, col + plus_c):
-            now_alpha = ord(board[row + plus_r][col + plus_c]) - 65
+    for dr, dc in deg:
+        nr = row + dr
+        nc = col + dc
+        if not is_wall(nr, nc):
+            now_alpha = ord(board[nr][nc]) - 65
             if not visited[now_alpha]:
-                dfs([row + plus_r, col + plus_c], visited)
+                dfs([nr, nc], visited, cnt)
     cnt -= 1
     visited[alpha] = 0
 
@@ -24,15 +28,11 @@ def dfs(node, visited):
 r, c = map(int, input().split())
 board = []
 visited = [0 for _ in range(26)]
-deg = [[-1, 0], [1, 0], [0, -1], [0, 1]]
 for i in range(r):
-    tmp = []
-    for j in input():
-        tmp.append(j)
-    board.append(tmp)
+    board.append(sys.stdin.readline()[:-1])
 
 # print(board)
 
-cnt, result = 0, 0
-dfs([0, 0], visited)
+result = 0
+dfs([0, 0], visited, 0)
 print(result)
