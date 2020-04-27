aafc5be90b7ca4eac4c550f46b536662a42e6fdd
sweetrina096 <gpfhddl09@gmail.com>
2020-04-27 15:54:06 +0900

    초기 가능 값 설정. 백트래킹 간과. 수정필요

--- Baekjoon/simulation/2580_스도쿠.py
+++ Baekjoon/simulation/2580_스도쿠.py
@@ -2,51 +2,46 @@ import sys
 sys.stdin = open("2580_input.txt")
 
 
-def find_row(r, c):
-    visited = [0 for _ in range(10)]
-    for i in range(9):
-        if i == c:
-            continue
-        if board[r][i]:
-            visited[board[r][i]] = 1
-        else:
-            return 0
-    for i in range(1, 10):
-        if not visited[i]:
-            return i
-
-def find_col(r, c):
-    visited = [0 for _ in range(10)]
-    for i in range(9):
-        if i == r:
-            continue
-        if board[i][c]:
-            visited[board[i][c]] = 1
-        else:
-            return 0
-    for i in range(1, 10):
-        if not visited[i]:
-            return i
-
-def find_square(r, c):
-    visited = [0 for _ in range(10)]
-    sr = (r//3) * 3
-    sc = (c//3) * 3
-    for nr in range(sr, sr+3):
-        for nc in range(sc, sc+3):
-            if nr == r and nc == c:
-                continue
-            if board[nr][nc]:
-                visited[board[nr][nc]] = 1
-            else:
-                return 0
-    for i in range(1, 10):
-        if not visited[i]:
-            return i
-
 board = []
 for _ in range(9):
     board.append(list(map(int, input().split())))
+
+rows = dict()
+cols = dict()
+squares = dict()
+
+for i in range(9):
+    vr = [0 for _ in range(10)]
+    vc = [0 for _ in range(10)]
+    for j in range(9):
+        vr[board[i][j]] = 1
+        vc[board[j][i]] = 1
+    tr = []
+    tc = []
+    for k in range(1, 10):
+        if not vr[k]:
+            tr.append(k)
+        if not vc[k]:
+            tc.append(k)
+    rows[i] = tr
+    cols[i] = tc
+# print(rows)
+# print(cols)
+
+
+for r in range(3):
+    for c in range(3):
+        visited = [0 for _ in range(10)]
+        for nr in range(r*3, r*3 + 3):
+            for nc in range(c*3, c*3 + 3):
+                if board[nr][nc]:
+                   visited[board[nr][nc]] = 1
+        temp = []
+        for i in range(1, 10):
+            if not visited[i]:
+                temp.append(i)
+        squares[(r, c)] = temp
+# print(squares)
 
 stack = []
 for r in range(9):
@@ -57,24 +52,41 @@ for r in range(9):
 
 while stack:
     r, c = stack.pop(0)
+    sr = r//3
+    sc = c//3
     # print(r, c)
     # for line in board:
     #     print(line)
-    val = find_row(r, c)
-    if val:
-        board[r][c] = val
-        continue
+    val = 0
+    if rows.get(r) and len(rows[r]) == 1:
+        val = rows.pop(r)[0]
+    elif cols.get(c) and len(cols[c]) == 1:
+        val = cols.pop(c)[0]
+    elif squares.get((sr, sc)) and len(squares[(sr, sc)]) == 1:
+        val = squares.pop((sr, sc))[0]
 
-    val = find_col(r, c)
-    if val:
-        board[r][c] = val
+    if not val:
+        stack.append((r, c))
         continue
+    board[r][c] = val
 
-    val = find_square(r, c)
-    if val:
-        board[r][c] = val
-        continue
-    stack.append((r, c))
+    if rows.get(r):
+        if len(rows[r]) == 1 and val in rows[r]:
+            rows.pop(r)
+        elif val in rows[r]:
+            rows[r].remove(val)
+    if cols.get(c):
+        if len(cols[c]) == 1 and val in cols[c]:
+            cols.pop(c)
+        elif val in cols[c]:
+            cols[c].remove(val)
+    if squares.get((sr, sc)):
+        if len(squares[(sr, sc)]) == 1 and val in squares[(sr, sc)]:
+            squares.pop((sr, sc))
+        elif val in squares[(sr, sc)]:
+            squares[(sr, sc)].remove(val)
+
+
 
 for line in board:
     print(' '.join(map(str, line)))
